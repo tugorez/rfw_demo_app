f29a8ac80e180262ad2f31b1500583ec0ea33b5b
Juanjo Tugores <tugorez@gmail.com>
2024-01-13 12:58:56 -0800

    Add support for local widgets

---
+++ lib/infra/local_widgets.dart
@@ -0,0 +1,7 @@
+import 'package:rfw/rfw.dart';
+
+LocalWidgetLibrary createLocalWidgets() =>
+    LocalWidgetLibrary(_localWidgetLibrary);
+
+// Add your local widgets here.
+final Map<String, LocalWidgetBuilder> _localWidgetLibrary = {};
--- lib/infra/runtime.dart
+++ lib/infra/runtime.dart
@@ -1,5 +1,6 @@
 import 'package:rfw/rfw.dart';
 import 'package:rfw/formats.dart' show parseLibraryFile;
+import 'package:rfw_demo_app/infra/local_widgets.dart';
 
 const FullyQualifiedWidgetName mainWidget = FullyQualifiedWidgetName(
   _mainLibraryName,
@@ -8,7 +9,8 @@ const FullyQualifiedWidgetName mainWidget = FullyQualifiedWidgetName(
 
 Runtime createRuntime() => Runtime()
   ..update(const LibraryName(<String>['core', 'widgets']), createCoreWidgets())
-  ..update(const LibraryName(<String>['material']), createMaterialWidgets());
+  ..update(const LibraryName(<String>['material']), createMaterialWidgets())
+  ..update(const LibraryName(<String>['local']), createLocalWidgets());
 
 extension UpdateMainLibrary on Runtime {
   void updateMainLibrary(String library) {
