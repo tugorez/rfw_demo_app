1d470758c42b901da7831c1d063fab4df097089f
Juan Tugores <tugorez@google.com>
2024-03-11 22:12:44 -0700

    Add animations local widgets

--- lib/infra/local_widgets.dart
+++ lib/infra/local_widgets.dart
@@ -1,8 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:rfw/rfw.dart';
 
 LocalWidgetLibrary createLocalWidgets() =>
     LocalWidgetLibrary(_localWidgetLibrary);
 
-// Add your local widgets here.
-final Map<String, LocalWidgetBuilder> _localWidgetLibrary =
-    <String, LocalWidgetBuilder>{};
+final Map<String, LocalWidgetBuilder> _localWidgetLibrary = <String, LocalWidgetBuilder>{
+  'FlutterLogo': (BuildContext context, DataSource source) {
+    return FlutterLogo(size: source.v<double>(['size']));
+  },
+  'AnimatedAlign': (BuildContext context, DataSource source) {
+    return AnimatedAlign(
+      alignment: ArgumentDecoders.alignment(source, ['alignment'])!,
+      duration: ArgumentDecoders.duration(source, ['duration'], context),
+      child: source.child(['child']),
+    );
+  },
+  'AnimatedDefaultTextStyle': (BuildContext context, DataSource source) {
+    return AnimatedDefaultTextStyle(
+      style: ArgumentDecoders.textStyle(source, ['style'])!,
+      duration: ArgumentDecoders.duration(source, ['duration'], context),
+      child: source.child(['child']),
+    );
+  },
+  'AnimatedScale': (BuildContext context, DataSource source) {
+    return AnimatedScale(
+      scale: source.v<double>(['scale'])!,
+      duration: ArgumentDecoders.duration(source, ['duration'], context),
+      child: source.child(['child']),
+    );
+  },
+};
